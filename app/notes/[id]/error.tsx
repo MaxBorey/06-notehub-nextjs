63eab20f65b758907c8c4458f9e47431ccf2b05b
maxborey <maxborey@gmail.com>
2025-06-21 11:20:35 +0300

    finish_0621_5

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -9,7 +9,7 @@ export default function Error({ error, reset }: Props) {
   return (
     <div>
       <p>Could not fetch note details. {error.message}</p>
-      <button onClick={reset}>Try again</button>
+      <button onClick={reset}>Try again...</button>
     </div>
   );
 }
